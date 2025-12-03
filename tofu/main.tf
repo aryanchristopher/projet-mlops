e20cd08ec8f89353822e376bb29b1b4c6129b288
Aryan CHRISTOPHER <183614519+aryanchristopher@users.noreply.github.com>
2025-12-03 23:16:21 +0100

    Infra: Génération automatique de la clé SSH pour Ansible

--- tofu/main.tf
+++ tofu/main.tf
@@ -11,3 +11,22 @@ terraform {
 provider "aws" {
   region = "eu-west-3"
 }
+
+# 1. On génère une nouvelle clé SSH sécurisée automatiquement
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# 2. On envoie la partie "publique" de la clé à AWS
+resource "aws_key_pair" "deployer" {
+  key_name   = "mlops-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+# 3. On sauvegarde la partie "privée" sur ton PC pour qu'Ansible l'utilise
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/../ansible/id_rsa.pem"
+  file_permission = "0600" # Très important : lecture seule pour toi (sécurité Linux)
+}
